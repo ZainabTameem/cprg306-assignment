79a237e03dbae9254884804cea55b917cbb8d955
ZainabTameem <Tameemz2005@gmail.com>
2025-09-19 18:39:44 -0600

    updated project files with latest changes

---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+'use client';
+
+import Link from "next/link";
+import React from "react";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Zainab Tameem</p>
+      <p>
+        <Link href="https://github.com/ZainabTameem/cprg306-assignment" target="_blank">
+          GitHub: ZainabTameem/cprg306-assignments
+        </Link>
+      </p>
+    </div>
+  );
+}
